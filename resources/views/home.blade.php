55bf58a0d43d23b5121defda7e94daee7ed3d97a
Samuel Graterol <samuelgraterol12@gmail.com>
2022-04-07 17:02:45 -0400

    Planteo del sistema en proceso...

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page', ['use_ico_only' => true, 'use_full_favicon' => false])
+@section('plugins.Datatables', true)
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Inicio</h1>
+@stop
+
+@section('content')
+
+
+
+
+
+
+@stop
+
+
+@section('js')
+
+@stop
