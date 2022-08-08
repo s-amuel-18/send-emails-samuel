d3628d6140ab9bf519b570e9ad2d1f03f8a63ac6
branmarvel <88461465+branmarvel@users.noreply.github.com>
2022-08-08 10:10:42 -0400

    correcciones email

--- resources/views/emails/fluxel_code_service.blade.php
+++ resources/views/emails/fluxel_code_service.blade.php
@@ -7,98 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fluxel Code</title>
 </head>
-<style>
-    * {
-        font-family: sans-serif;
-        margin: 0;
-        box-sizing: border-box;
-        padding: 0;
-    }
-
-
-    .body {
-        background-color: rgb(250, 250, 250);
-        padding: 20px;
-    }
-
-    html {
-        box-sizing: border-box;
-    }
-
-    p {
-        margin-bottom: 7px;
-    }
-
-    *,
-    *:before,
-    *:after {
-        box-sizing: inherit;
-    }
-
-    .center {
-        text-align: center;
-    }
-
-    /* .p-15px {
-        padding: 15px;
-    } */
-
-    .container-mail {
-        max-width: 540px;
-        background-color: white;
-        margin: auto;
-    }
-
-
-    .card {
-        padding-left: 30px;
-        padding-right: 30px;
-        padding-bottom: 30px;
-
-
-    }
-
-
-    .t-gray {
-        color: gray;
-    }
-
-    .logo {
-        height: 60px;
-        vertical-align: middle;
-        display: block;
-        color: white;
-        background-color: #672fa8;
-    }
-
-    .d-flex {
-        display: flex;
-        align-items: center;
-
-
-    }
-
-    .j-center {
-        justify-content: center;
-    }
-
-
-    .img-40sq {
-        /* width: 50px; */
-        height: 20px;
-    }
-
-    .img-100sq {
-        width: 70px;
-        margin-left: 10px;
-        /* padding: 5px; */
-    }
-
-    .py-2 {
-        padding-left: 5px;
-        padding-right: 5px;
-    }
-</style>
 
 <body>
     <div style="   background-color: rgb(250, 250, 250);
@@ -118,10 +26,10 @@
                 vertical-align: middle;
                 display: block;
                 color: white;
-                background-color: #672fa8; display: flex;
-        align-items: center;">
-                    <a href=""><img style="  width: 70px;
-                        margin-left: 10px;"
+                background-color: #672fa8;   ">
+                    <a href=""><img
+                            style="  width: 70px;
+                        margin-left: 10px; margin-top:15px"
                             src="https://www.negociaecuador.com/brandon-prueba/fluxel_code_xl.png" alt="">
                     </a>
                 </div>
@@ -136,21 +44,22 @@
                 vertical-align: middle;
                 display: block;
                 color: white;
-                background-color: #672fa8;         display: flex;
-        align-items: center; justify-content: center;">
+                background-color: #672fa8;">
+
                     <a style="  padding-left: 5px;
                     padding-right: 5px;" target="_blank"
                         href="https://walink.co/c9af79">
-                        <img class="img-40sq" src="https://www.negociaecuador.com/brandon-prueba/ws.png" alt="Whatsapp">
+                        <img style="width: 20px ; margin-top: 15px;"
+                            src="https://www.negociaecuador.com/brandon-prueba/ws.png" alt="Whatsapp">
                     </a>
                     <a style="  padding-left: 5px;
                     padding-right: 5px;" target="_blank"
-                        href="mailto:fluxel.code@gmail.com"><img class="img-40sq"
+                        href="mailto:fluxel.code@gmail.com"><img style="width: 20px ; margin-top: 15px;"
                             src="https://www.negociaecuador.com/brandon-prueba/gmail.png" alt="gmail">
                     </a>
                     <a style="  padding-left: 5px;
                     padding-right: 5px;" target="_blank"
-                        href="https://www.instagram.com/fluxel_code"><img class="img-40sq"
+                        href="https://www.instagram.com/fluxel_code"><img style="width: 20px ; margin-top: 15px;"
                             src="https://www.negociaecuador.com/brandon-prueba/instagram.png" alt="instagram">
                     </a>
 
