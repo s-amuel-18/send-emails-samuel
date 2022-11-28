b334c5bf906219e1c4df2bb4bde2dc1d4d26ed59
s-amuel-18 <63316336+s-amuel-18@users.noreply.github.com>
2022-11-28 11:18:37 -0400

    rutas

--- app/Http/Controllers/api/SubjectController.php
+++ app/Http/Controllers/api/SubjectController.php
@@ -18,6 +18,35 @@ class SubjectController extends Controller
 
     public function store(Request $request)
     {
+        $data = request()->validate([
+            "name" => "required|max:100|string",
+        ]);
+
+        $name_categorie = $data["name"];
+        $categorie_type = Requirements::class;
+
+        $exist_categorie = Category::where("catgoriable_type", $categorie_type)
+            ->where("name", $name_categorie)
+            ->count();
+
+        if ($exist_categorie > 0) {
+            return response()->json(["message" => "El nombre de la categoría ya está registrado."], 403);
+        }
+
+        $name = $data["name"];
+
+        $category = auth()->user()->categories()->create([
+            "name" => $name, "catgoriable_type" => Requirements::class,
+            "catgoriable_type" => $categorie_type,
+        ]);
+
+        $response_data = [
+            "message" => "La categoría se ha registrado correctamente.",
+            "data_insert" => $category
+        ];
+
+
+        return response()->json($response_data, 200);
     }
 
     public function update(Type $var = null)
